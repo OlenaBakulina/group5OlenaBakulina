c1b73ebcf500865d6ae33fbe91ce1fe32e03e821
Olena <lenao2932009@gmail.com>
2020-08-19 22:46:36 +0300

    test22

--- src/main/java/Lesson3/A_Intro.java
+++ src/main/java/Lesson3/A_Intro.java
@@ -1,4 +1,7 @@
 package Lesson3;
 
 public class A_Intro {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
--- src/main/java/Lesson3/B_Byte.java
+++ src/main/java/Lesson3/B_Byte.java
@@ -1,4 +1,13 @@
 package Lesson3;
 
 public class B_Byte {
+    public static void main(String[] args) {
+        byte a = 12;
+        System.out.println(a);
+        byte b = 'a';
+        System.out.println(b);
+        byte c = 'A';
+        System.out.println(c);
+
+    }
 }
